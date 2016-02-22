74424f277ed6219cefb87623e422cc09f0ecff40
Karlkim Suwamongkol <contact@karlk.im>
2016-02-21 21:30:32 -0800

    install mongodb client

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import { MongoClient } from 'mongodb';
 
 let app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+(async () => {
+  let db = await MongoClient.connect(process.env.MONGO_URL);
+  
+  app.listen(3000, () => console.log('Listening on port 3000 - async'));
+})();
