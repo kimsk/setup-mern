e042ca02e91414c3abae9dd1ab7cf42807b1d445
Karlkim Suwamongkol <contact@karlk.im>
2016-02-21 21:16:38 -0800

    set up express and server

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ import ReactDOM  from 'react-dom';
 class Mern extends React.Component {
   render(){
     return (
-      <h1>Mongo, Express, React, Node</h1>
+      <h1>Mongo, Express, React, NodeJS</h1>
     )
   }
 }
