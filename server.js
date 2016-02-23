69f362836977a07fa1a4224b8ac0407590f870b8
Karlkim Suwamongkol <contact@karlk.im>
2016-02-22 21:57:31 -0800

    put it together

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ import express from 'express';
 import { MongoClient } from 'mongodb';
 
 let app = express();
+const port = 3000;
 
 app.use(express.static('public'));
 
 (async () => {
   let db = await MongoClient.connect(process.env.MONGO_URL);
-  
-  app.listen(3000, () => console.log('Listening on port 3000 - async'));
+
+  app.listen(port, () => console.log(`Listening on port ${port}`));
 })();
